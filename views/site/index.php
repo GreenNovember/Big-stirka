1ca94902429a25018bf333b660c3701c547e5281
exxxar <exxxar@gmail.com>
2016-01-23 19:16:21 +0300

    add html template to yii2 framework

---
+++ views/site/index.php
@@ -0,0 +1,217 @@
+            <section class="slider">
+                <div id="slides">
+                    <img src="img/1.jpg">
+                    <img src="img/2.jpg">
+                    <img src="img/3.jpg">
+                    <img src="img/4.jpg">
+                </div>
+            </section>
+
+            <section class="search">
+                <div class="etc">
+                    <p>
+                        НАЙДИТЕ НУЖНЫЙ ВАМ ТОВАР!
+                    </p>
+                    <form class="search-form" action="/searchitem" method="post">
+                        <input type="search" name="search" placeholder="Найди товар" pattern="[a-zA-Z0-9]+">
+                        <input class="one" type="submit" value="ПОИСК">
+                    </form>
+                </div>
+                <div class="arrow">
+                    <img src="img/arrow.png">
+                </div>
+            </section>
+
+            <section class="brand">
+                <div class="slider2">
+                    <div class="slider-item">
+                        <img src="img/nivea.png">
+                        <img src="img/dove.png">
+                        <img src="img/freken.png">
+                        <img src="img/lady.png">
+                        <img src="img/selpac.png">
+                        <img src="img/lenor.png">
+                        <img src="img/duru.png">
+                        <img src="img/sto.png">
+                        <img src="img/line.png">
+                        <img src="img/bella.png">
+                    </div>
+                    <div class="slider-item">
+                        <img src="img/nivea.png">
+                        <img src="img/dove.png">
+                        <img src="img/freken.png">
+                        <img src="img/lady.png">
+                        <img src="img/selpac.png">
+                        <img src="img/lenor.png">
+                        <img src="img/duru.png">
+                        <img src="img/sto.png">
+                        <img src="img/line.png">
+                        <img src="img/bella.png">
+                    </div>
+                    <div class="slider-item">
+                        <img src="img/nivea.png">
+                        <img src="img/dove.png">
+                        <img src="img/freken.png">
+                        <img src="img/lady.png">
+                        <img src="img/selpac.png">
+                        <img src="img/lenor.png">
+                        <img src="img/duru.png">
+                        <img src="img/sto.png">
+                        <img src="img/line.png">
+                        <img src="img/bella.png">
+                    </div>
+                </div>
+                <div class="left-arrow">
+                    <img src="img/left.png">
+                </div>
+                <div class="right-arrow">
+                    <img src="img/right.png">
+                </div>
+            </section>
+
+            <section class="catalog">
+                <h1>
+                    КАТАЛОГ
+                </h1>
+                <div class="pp powder">
+                    <h2>
+                        СТИРАЛЬНЫЕ<br>
+                        ПОРОШКИ
+                    </h2>
+                    <img src="img/poroshok.png">
+                </div>
+                <div class="pp shampoo">
+                    <h2>
+                        ШАМПУНИ
+                    </h2>
+                    <img src="img/shampuni.png">
+                </div>
+                <div class="pp toothpaste">
+                    <h2>
+                        ЗУБНЫЕ ПАСТЫ
+                    </h2>
+                    <img src="img/pasti.png">
+                </div>
+                <div class="pp gels-washing">
+                    <h2>
+                        ГЕЛИ ДЛЯ<br>
+                        СТИРКИ
+                    </h2>
+                    <img src="img/geli.png">
+                </div>
+                <div class="pp dishwashing">
+                    <h2>
+                        СРЕДСТВА ДЛЯ<br>
+                        МЫТЬЯ ПОСУДЫ
+                    </h2>
+                    <img src="img/posuda.png">
+                </div>
+
+                <div class="pp-info shampoo-info">			
+                    <div class="info-item">
+                        <div class="info-item-img">
+                            <img src="img/i1.png">
+                        </div>
+                        <div class="info-item-title">
+                            <h1>"Пучной Мишка" ЛАСКА НЕЖНЫЙ 0.5 Л</h1>
+                        </div>
+
+                        <div class="info-item-control">
+                            <div class="item-like"  title="Добавить в избранное" rel="tooltip"><img src="img/like-label.png"></div>
+                            <div class="item-to-wallet"  title="Добавить в корзину" rel="tooltip"><img src="img/wallet-label.png"></div>
+                            <div class="item-price"><p>1400р</p></div>
+                        </div>					
+                    </div>
+                    <div class="info-item">
+                        <div class="info-item-img">
+                            <img src="img/i2.png">
+                        </div>
+                        <div class="info-item-title">
+                            <h1>"Пучной Мишка" ЛАСКА НЕЖНЫЙ 0.5 Л</h1>
+                        </div>
+
+                       <div class="info-item-control">
+                            <div class="item-like"  title="Добавить в избранное" rel="tooltip"><img src="img/like-label.png"></div>
+                            <div class="item-to-wallet"  title="Добавить в корзину" rel="tooltip"><img src="img/wallet-label.png"></div>
+                            <div class="item-price"><p>1400р</p></div>
+                        </div>			
+                    </div>
+                    <div class="info-item">
+                        <div class="info-item-img">
+                            <img src="img/i3.png">
+                        </div>
+                        <div class="info-item-title">
+                            <h1>"Пучной Мишка" ЛАСКА НЕЖНЫЙ 0.5 Л</h1>
+                        </div>
+
+                       <div class="info-item-control">
+                            <div class="item-like"  title="Добавить в избранное" rel="tooltip"><img src="img/like-label.png"></div>
+                            <div class="item-to-wallet"  title="Добавить в корзину" rel="tooltip"><img src="img/wallet-label.png"></div>
+                            <div class="item-price"><p>1400р</p></div>
+                        </div>			
+                    </div>
+                    <div class="info-item">
+                        <div class="info-item-img">
+                            <img src="img/i4.png">
+                        </div>
+                        <div class="info-item-title">
+                            <h1>"Пучной Мишка" ЛАСКА НЕЖНЫЙ 0.5 Л</h1>
+                        </div>
+
+                       <div class="info-item-control">
+                            <div class="item-like"  title="Добавить в избранное" rel="tooltip"><img src="img/like-label.png"></div>
+                            <div class="item-to-wallet"  title="Добавить в корзину" rel="tooltip"><img src="img/wallet-label.png"></div>
+                            <div class="item-price"><p>1400р</p></div>
+                        </div>				
+                    </div>
+                    <div class="info-item">
+                        <div class="info-item-img">
+                            <img src="img/i5.png">
+                        </div>
+                        <div class="info-item-title">
+                            <h1>"Пучной Мишка" ЛАСКА НЕЖНЫЙ 0.5 Л</h1>
+                        </div>
+
+                       <div class="info-item-control">
+                            <div class="item-like"  title="Добавить в избранное" rel="tooltip"><img src="img/like-label.png"></div>
+                            <div class="item-to-wallet"  title="Добавить в корзину" rel="tooltip"><img src="img/wallet-label.png"></div>
+                            <div class="item-price"><p>1400р</p></div>
+                        </div>			
+                    </div>							
+                </div>
+            </section>
+
+            <section class="social-networks">
+                <div class="sn facebook">
+                </div>
+                <div class="sn google">
+                </div>
+                <div class="sn twitter">
+                </div>
+                <div class="sn vk">
+                </div>
+                <div class="sn instagram">
+                </div>
+            </section>
+
+            <section class="map">
+                <div class="above-map">
+                    <h1>
+                        МЫ НАХОДИМСЯ СОВСЕМ РЯДОМ
+                    </h1>
+                    <div class="arrow1">
+                        <img src="img/arrow.png">
+                    </div>
+                </div>
+                <div id="googleMap"></div>
+                <div class="bottles">
+                    <img src="img/bottle.png">
+                </div>
+                <div class="for-connect">
+                    <h1>
+                        СВЯЖИСЬ С НАМИ
+                    </h1>
+                </div>
+            </section>
+
+			
